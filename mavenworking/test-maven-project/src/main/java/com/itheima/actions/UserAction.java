e54e15835a70d69b4055be91d91d8d2b703c7b3d
gitlip1957 <2233193308@qq.com>
2019-02-20 17:44:52 +0800

    test maven project struts2

---
+++ mavenworking/test-maven-project/src/main/java/com/itheima/actions/UserAction.java
@@ -0,0 +1,29 @@
+package com.itheima.actions;
+
+public class UserAction {
+	private String name;
+	private Integer age;
+
+	public String execute() {
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		return "success";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
---
+++ mavenworking/test-maven-project/src/main/webapp/welcome.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"   pageEncoding="UTF-8" isELIgnored="false"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+    姓名：${name }<br/>
+    年龄：${age }
+</body>
+</html> 
