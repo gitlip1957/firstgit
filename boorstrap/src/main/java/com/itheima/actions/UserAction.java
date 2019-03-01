4f88440d2351008885bba5cd4c67e6fb3bbc01c6
gitlip1957 <2233193308@qq.com>
2019-03-01 11:10:13 +0800

    dev update userAction

--- boorstrap/src/main/java/com/itheima/actions/UserAction.java
+++ boorstrap/src/main/java/com/itheima/actions/UserAction.java
@@ -8,7 +8,7 @@ public class UserAction {
 		System.out.println("name:" + name);
 		System.out.println("age:" + age);
 		
-		System.out.println("=========aaagitceshi===============");
+		System.out.println("======== aaa   aaa  itceshi===============");
 		return "success";
 	}
 
